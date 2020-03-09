686071a09775db127c2c4f784e328e50cab13929
gneissi <57685971+gneissi@users.noreply.github.com>
2020-03-09 13:50:48 +0200

    a
    a

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
+
+
+<html>
+    <head></head>
+    <body>
 <?php
-
-    if(!empty($_SERVER["HTTP_CLIENT_IP"])){
-        //ip from share internet
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }elseif(!empty($_SERVER["HTTP_X_FORWARDED_FOR"])){
-        //ip pass from proxy
-        $ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
-    }else{
-        $ip = $_SERVER["REMOTE_ADDR"];
-    }
-    return $ip;
-
-echo $ip;
+		function get_ip(){
+			if(isset($_SERVER['HTTP_CLIENT_IP'])){
+				//ip from share internet
+				return $_SERVER['HTTP_CLIENT_IP'];
+			}elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+				//ip pass from proxy
+				return $_SERVER['HTTP_X_FORWARDED_FOR'];
+			}else{
+				return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
+			}
+			return $ip;
+		}
+		$ip = get_ip();
+		echo $ip;
 ?> 
+    </body>
+</html>
